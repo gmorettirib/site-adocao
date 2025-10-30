caaf3556a8560e247335dad97a6064e518e7eaba
Felipejbm <felipejbmello.etec@gmai.com>
2025-10-30 09:11:13 -0300

    menu pronto e inicio main

---
+++ frontend-adocao/src/components/home.tsx
@@ -0,0 +1,26 @@
+import "../styles/home.css";
+
+export default function HomePage() {
+  return (
+    <body>
+      <main>
+        <div className="container text-center">
+          <div className="row">
+            <div className="col">
+              <h1>ENCONTRE SEU COMPANHEIRO HOJE</h1>
+              <h2>Faça um animal feliz, aqui voce encontra diversos animais que foram encontrados, cuidados e que agora estao procurando um lar!</h2>
+            </div>
+            <div className="card">
+                <h1>oi</h1>
+            </div>
+            <div className="col">
+              <div className="img">
+                <img src="/public/img/cachorro1.png" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </body>
+  );
+}
--- frontend-adocao/src/components/menuHome.tsx
+++ frontend-adocao/src/components/menuHome.tsx
@@ -3,25 +3,26 @@ import "../styles/menuHome.css";
 export default function Header() {
   return (
     <header>
-      <nav>
-        <ol>
-          <a href="#">ADOCÃO</a>
-        </ol>
-        <ul>
-          <ol>
-            <a href="#">SOBRE O PROJETO</a>
-          </ol>
-          <ol>
-            <a href="#">ADOTAR</a>
-          </ol>
-          <ol>
-            <a href="#">CADASTRAR-SE</a>
-          </ol>
-          <ol>
-            <a href="#">LOGIN</a>
-          </ol>
-        </ul>
-      </nav>
-    </header>
+      
+          <nav>
+            <ol>
+              <a href="#">ADOCÃO</a>
+            </ol>
+            <ul>
+              <ol>
+                <a href="#">SOBRE O PROJETO</a>
+              </ol>
+              <ol>
+                <a href="#">ADOTAR</a>
+              </ol>
+              <ol>
+                <a href="#">LOGIN</a>
+              </ol>
+              <ol>
+                <a href="#">CADASTRAR-SE</a>
+              </ol>
+            </ul>
+          </nav>
+        </header>
   );
 }
